4fd60ad50ec59951b046d8c9e9be5b3c7915e527
Noonsoo <nwafornonso70@gmail.com>
2025-06-10 05:39:53 +0100

    Initial commit: added frontend and backend

---
+++ api/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  deleteUser,
+  getUsers,
+  getUser,
+  updateUser,
+} from "../controller/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.put("/:id", verifyUser, updateUser);
+
+router.delete("/:id", verifyUser, deleteUser);
+
+router.get("/:id", verifyUser, getUser);
+
+router.get("/", verifyAdmin, getUsers);
+
+export default router;
